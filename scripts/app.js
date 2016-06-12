98622f324ac95373f72c99fab5b03491e9e19c4b
Derrick Bentley <bentleyd@mail.gvsu.edu>
2016-06-12 01:51:49 -0400

    yeoman angular tutorial

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mytodoApp
+ * @description
+ * # mytodoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mytodoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable',
+    'LocalStorageModule'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/newPage', {
+        templateUrl: 'views/newpage.html',
+        controller: 'NewpageCtrl',
+        controllerAs: 'newPage'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }).config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+}]);
---
+++ scripts/controllers/newpage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:NewpageCtrl
+ * @description
+ * # NewpageCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('NewpageCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
